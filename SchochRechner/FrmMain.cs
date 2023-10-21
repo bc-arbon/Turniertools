d3fc6c18ad974b84e02fb649ffb5bf65bf2411ec
Daniel Hafner <dani@danionnet.ch>
2023-10-21 07:53:22 +0200

    example data loading removed

--- SchochRechner/FrmMain.cs
+++ SchochRechner/FrmMain.cs
@@ -127,7 +127,7 @@ namespace SchochRechner
         {
             this.schochManager.Load();
             //this.schochManager.AddExampleData();
-            this.schochManager.AddExampleData2();
+            //this.schochManager.AddExampleData2();
 
             this.frmDisplay.Show();
             this.ShowAll();
